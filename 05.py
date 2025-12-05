0153032762371bf76cfc16512fc694e5ef5f3469
marin1401 <58388551+marin1401@users.noreply.github.com>
2025-12-05 18:36:39 +0100

    Add files via upload

---
+++ 05.py
@@ -0,0 +1,46 @@
+# Day 05
+
+with open('./05.txt') as my_input:
+    input_string = my_input.read()
+
+def join_ranges(first_id_check, last_id_check, id_range):
+    for first_id, second_id in id_range:
+        if first_id < first_id_check and second_id >= first_id_check:
+            if second_id > last_id_check:
+                return (first_id, second_id)
+            else:
+                return (first_id, last_id_check)
+        elif second_id > last_id_check and first_id <= last_id_check:
+            return (min(first_id, first_id_check), second_id)
+    return False
+
+fresh_ids, available_ids = input_string.split("\n\n")
+
+fresh_ids = {tuple(map(int, ids.split("-"))) for ids in fresh_ids.splitlines()}
+
+# Part 1
+
+fresh_available_ids = set()
+for available_id in map(int, available_ids.splitlines()):
+    for first_id, last_id in fresh_ids:
+        if first_id <= available_id <= last_id:
+            fresh_available_ids.add(available_id)
+
+print(len(fresh_available_ids))
+
+# Part 2
+
+while True:
+    for first_id, last_id in list(fresh_ids):
+        if (new_range := join_ranges(first_id, last_id, fresh_ids)):
+            fresh_ids.remove((first_id, last_id))
+            fresh_ids.add(new_range)
+            break
+    else:
+        break
+
+counter = 0
+for first_id, last_id in fresh_ids:
+    counter += last_id - first_id + 1
+
+print(counter)
