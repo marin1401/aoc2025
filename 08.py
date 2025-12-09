7bf831dc09e317ef120b88f339fb3dcc60be327a
marin1401 <58388551+marin1401@users.noreply.github.com>
2025-12-09 18:03:52 +0100

    Update 08.py

--- 08.py
+++ 08.py
@@ -5,7 +5,7 @@ import math
 with open('./08.txt') as my_input:
     input_lines = my_input.readlines()
 
-def get_3d_distance(box_1, box_2):
+def get_3d_distance_squared(box_1, box_2):
     return sum((box_1[i] - box_2[i])**2 for i in range(3))
 
 boxes = [tuple(map(int, line.split(','))) for line in input_lines]
@@ -13,7 +13,7 @@ boxes = [tuple(map(int, line.split(','))) for line in input_lines]
 distances = []
 for i, box_1 in enumerate(boxes[:-1]):
     for box_2 in boxes[i+1:]:
-        distances.append((get_3d_distance(box_1, box_2), box_1, box_2))
+        distances.append((get_3d_distance_squared(box_1, box_2), box_1, box_2))
 
 circuits = []
 for pair, (distance, box_1, box_2) in enumerate(sorted(distances), start=1):
@@ -43,3 +43,4 @@ print(math.prod(circuits_sizes[:3]))
 # Part 2
 
 print(math.prod(x_coords))
+
